71dad446430733841553b3adf110ded1d22d3d38
Naresh Rajendra Shah <nareshshah139@gmail.com>
2016-05-13 01:44:56 +0200

    Create pysparkConv.py

---
+++ NLP/TextualEntailmentAssignment/pysparkConv.py
@@ -0,0 +1,22 @@
+pyspark --packages com.databricks:spark-xml_2.11:0.3.3
+
+
+import xml.etree.ElementTree as ET
+from pyspark.sql import Row
+
+def xmltordd(xmlstring)
+  dict = {"entailment":None, "text":None,"task":None,"pair" : None,"hypothesis":None}
+  tree = ET.parse(xmlstring)
+  root = tree.getroot()
+  for child in root:
+    dict["entailment"] = child.attrib.get("entailment")
+    dict["text"]= child.find("t").text
+    dict["task"]= child.attrib.get("task")
+    dict["pair"]= child.attrib.get("pair")
+    dict["hypothesis"] = child.find("h").text
+  
+    rdd1 = sc.parallelize(dict)
+    return rdd1
+
+
+rdd1 = sc.textFile().map(parse_xml_to_row)
