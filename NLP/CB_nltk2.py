f741ddc086f1ba996ea47cf42a3b42755cec1406
Christopher Brandenburg <ccbrandenburg@me.com>
2016-05-21 16:15:52 +0200

    Add new Spell Checker file
    - Fixed Dataframe for word_dist
    - Created Frequency dataframe for Brown. Could figure out how to lowercase all of brown words
    - trying to join the two dataframes but have problems (please check)

---
+++ NLP/CB_nltk2.py
@@ -0,0 +1,52 @@
+from nltk.corpus import brown
+from nltk.corpus import stopwords
+import nltk
+from nltk.metrics import *
+import pandas as pd
+
+sentence = input("Give me a sentence to check for spelling mistakes > ")
+tokens = nltk.word_tokenize(sentence.lower())
+words = set(brown.words())
+words_brown = brown.words()
+words_brown = [word for word in words_brown if len(word) > 1]
+words_brown = [word for word in words_brown if word not in stopwords.words('english')]
+
+#identify incorrectly spelled words within the sentence
+incorrect_words = []
+def check_spelling():
+    for word in tokens:
+        #check whether word is in wordnet (this doesnt include stopwords) so also check whether it appears in stopwords, in this case dont show.
+        if word not in words and word not in stopwords.words('english'):
+            incorrect_words.append(word)
+    return(incorrect_words)
+check_spelling()
+print(incorrect_words)
+
+#compute the edit_distance between incorrectly spelled words and the words within brown corpus
+word_dist = []
+for item in incorrect_words:
+    for word in words:
+        if(edit_distance(item,word) < 3):
+            word_dist.append({'item':item,'word':word,'distance':edit_distance(item,word)})
+word_dist = pd.DataFrame(word_dist)
+print(word_dist)
+
+#todo
+#1 create dataframe with word and frequency in brown
+# Output frequent words
+fdist = nltk.FreqDist(words_brown)
+word_freq = []
+for word, frequency in fdist.most_common(): #change the 100 to include more
+    word_freq.append({'words':word, 'frequency':frequency})
+word_freq = pd.DataFrame(word_freq)
+#print(word_freq.head(5))
+
+#try to work out this join
+df = word_dist.join(word_freq, on=['word'], how='outer')
+print(df.head(5))
+#Idea: additioanl feature to ensure we propose the correct word could be to look at the length of the word and give higher importance to words that have the same length in df['item'] and df['word']. Works well for "todya" does not work well for "helo"
+
+
+
+
+#test helo hello this is how it goes todya
